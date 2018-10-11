15e287b762079252051e99cbb9731b1b5bc680eb
Artur Kosorz <a.kosorz@exlabs.pl>
2018-10-11 21:55:48 +0200

    Kata #18

---
+++ 18.js
@@ -0,0 +1,18 @@
+// Coding in function splitAndMerge, function accept 2 parameters:str and sp. str is a sentence. sp is a char as separator.
+//
+//     First we need to divide the sentence into words(Use separator space); and then divide each word into characters(Use separator empty string); and then merge each characters with the specified sp; at last merge all the words(Use separator space) and return it.
+//
+//     for example:
+//
+//     splitAndMerge("My name is John"," ") should return "M y n a m e i s J o h n"
+// splitAndMerge("My name is John","-") should return "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!",".") should return "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!",",") should return "H,e,l,l,o W,o,r,l,d,!"
+
+function splitAndMerge(str,sp){
+    var outPutArray = []
+    var str = str.split(" ").forEach((el)=>{
+        outPutArray.push(el.split('').join(sp))
+    })
+    return outPutArray.join(' ')
+}
