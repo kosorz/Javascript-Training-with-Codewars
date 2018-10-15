e2ad9dc7ba5a64280d8434724f3241aefb7fc71d
Artur Kosorz <a.kosorz@exlabs.pl>
2018-10-15 22:54:37 +0200

    Kata #32

---
+++ 32.js
@@ -0,0 +1,25 @@
+// Task
+// Coding in function roundIt. function accept 1 parameter n. It's a number with a decimal point. Please use different methods based on the location of the decimal point, turn the number into an integer.
+//
+// If the decimal point is on the left side of the number (that is, the count of digits on the left of the decimal point is less than that on the right), Using ceil() method.
+//
+// roundIt(3.45) should return 4
+// If the decimal point is on the right side of the number (that is, the count of digits on the left of the decimal point is more than that on the right), Using floor() method.
+//
+// roundIt(34.5) should return 34
+// If the decimal point is on the middle of the number (that is, the count of digits on the left of the decimal point is equals that on the right), Using round() method.
+//
+// roundIt(34.56) should return 35
+
+
+function roundIt(n){
+    var numberLength = n.toString().length-1
+    var commaPosition = n.toString().split('').indexOf('.')
+    if(numberLength/2 < commaPosition) {
+        return Math.floor(n);
+    } else if(numberLength/2 > commaPosition ){
+        return Math.ceil(n);
+    } else {
+        return Math.round(n);
+    }
+}
